78a7df60994428f5b415024dc2ab61fc36a4dcdc
Payvand Moaddel <payvand.moaddel@genesys.com>
2019-04-22 11:48:21 -0500

    dev save

--- crawler.js
+++ crawler.js
@@ -1,4 +1,4 @@
-var request = require('request');
+var request = require('request-promise');
 var cheerio = require('cheerio');
 var URL = require('url-parse');
 var fs = require('fs');
@@ -6,7 +6,7 @@ var fs = require('fs');
 
 var START_URL = "https://www.warhammer-community.com/2019/04/16/the-rumour-engine-tuesday-16th-april/" //"https://www.warhammer-community.com/?s=Rumour";
 var SEARCH_WORD = "rumour";
-var MAX_PAGES_TO_VISIT = 10000;
+var MAX_PAGES_TO_VISIT = 100;
 
 var pagesVisited = {};
 var numPagesVisited = 0;
@@ -17,53 +17,56 @@ var rumourImages = [];
 
 pagesToVisit.push(START_URL);
 crawl();
+saveResults();
 
-function crawl() {
-    if(numPagesVisited >= MAX_PAGES_TO_VISIT) {
-        console.log("Reached max limit of number of pages to visit.");
-        saveResults();
-        return;
+function async crawl() {
+    while (true) {
+        if(numPagesVisited >= MAX_PAGES_TO_VISIT) {
+            console.log("Reached max limit of number of pages to visit.");
+            break;
+        }
+        var nextPage = pagesToVisit.pop();
+
+        if (!nextPage) {
+            // we are done
+            console.log('no more pages to visit');
+            break;
+        }
+
+        if (nextPage in pagesVisited) {
+            // We've already visited this page, so repeat the crawl
+            continue;
+        }
+
+        if (nextPage.includes('.pdf')) {
+            // don't look at all the pdfs on the website
+            continue;
+        }
+
+        // New page we haven't visited
+        await visitPage(nextPage);
     }
-    // console.log('pagesToVisit', pagesToVisit);
-    var nextPage = pagesToVisit.pop();
-
-    if (!nextPage) {
-        // we are done
-        console.log('no more pages to visit');
-        saveResults();
-        return;
-    }
-
-    if (nextPage in pagesVisited) {
-        // We've already visited this page, so repeat the crawl
-        return crawl();
-    }
-
-    if (nextPage.includes('.pdf')) {
-        // don't look at all the pdfs on the website
-        return crawl();
-    }
-
-    // New page we haven't visited
-    visitPage(nextPage, crawl);
 }
 
-function visitPage(url, callback) {
+
+
+function async visitPage(url) {
     // Add page to our set
     pagesVisited[url] = true;
     numPagesVisited++;
 
+
     // Make the request
-    console.log("Visiting page " + url);
-    request(url, function(error, response, body) {
-        // Check status code (200 is HTTP OK)
-        console.log("Status code: " + response.statusCode);
-        if(response.statusCode !== 200) {
-            callback();
-            return;
+    console.log("Visiting page " + url, 'num visited', numPagesVisited);
+
+    var options = {
+        uri: url,
+        transform: function (body) {
+            return cheerio.load(body);
         }
-        // Parse the document body
-        var $ = cheerio.load(body);
+    };
+    return request(options)
+    .then(($) => {
         var rumourImage = checkForRumourImage($);
         if(rumourImage) {
             console.log('Rumour Image found:' + rumourImage);
@@ -73,6 +76,8 @@ function visitPage(url, callback) {
         collectInternalLinks($);
         // In this short program, our callback is just calling crawl()
         callback();
+    }).catch((err) => {
+        console.error('error getting page', err);
     });
 }
 
