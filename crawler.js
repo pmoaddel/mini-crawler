b5e56abc83720ba778fefc2b4f06de2427900dda
Payvand Moaddel <payvand.moaddel@genesys.com>
2019-04-26 15:47:51 -0500

    improve rumor page identification

--- crawler.js
+++ crawler.js
@@ -3,16 +3,21 @@ var cheerio = require('cheerio');
 var URL = require('url-parse');
 var fs = require('fs');
 
-
-const START_URL = "https://www.warhammer-community.com/2019/04/16/the-rumour-engine-tuesday-16th-april/" //"https://www.warhammer-community.com/?s=Rumour";
-const MAX_PAGES_TO_VISIT = 1000;
+const START_URL = "https://www.warhammer-community.com/2019/02/12/the-rumour-daemon-engine/" //"https://www.warhammer-community.com/?s=Rumour";
+const MAX_PAGES_TO_VISIT = process.argv[2] || 10;
 
 var pagesVisited = {};
 var numPagesVisited = 0;
 var pagesToVisit = [];
 var url = new URL(START_URL);
 var baseUrl = url.protocol + "//" + url.hostname;
-var rumourImages = {};
+var rumorPages = [];
+
+console.log(' |||||||||||||||||||||||||||||||||||||||||||||||||');
+console.log(' ||||||| LOOKING FOR THOSE HOT MINI RUMORS |||||||');
+console.log(' |||||||||||||||||||||||||||||||||||||||||||||||||');
+console.log('START URL:', START_URL);
+console.log('MAX PAGES TO VISIT:', MAX_PAGES_TO_VISIT);
 
 process.on('SIGINT', function () {
     console.log('execution interrupted');
@@ -74,10 +79,19 @@ async function visitPage(url) {
     };
     try {
         let $ = await request(options);
-        let rumourImage = checkForRumourImage($);
-        if(rumourImage) {
-            console.log('Rumour Image found:' + rumourImage);
-            rumourImages.push(rumourImage);
+        if(isRumorPage($)) {
+            let title = $('head > title').text();
+            console.log('Rumor Page Found:' + title);
+            let rumorPageData = {
+                imageUrls: getRumorImages($),
+                pageUrl: url,
+                title,
+                description: $('.post-body__content').find('p').text(),
+                datePosted: $('p.post-body__date').text()
+            }
+
+            rumorPages.push(rumorPageData);
+
         }
         collectInternalLinks($);
 
@@ -91,12 +105,53 @@ function searchForWord($, word) {
   return(bodyText.indexOf(word.toLowerCase()) !== -1);
 }
 
-function checkForRumourImage($) {
-    let rumourImage = $("img[src*=RumourEngine]");
-    if (rumourImage.length) {
-        return rumourImage[0].attribs.src;
+function getRumorImages($) {
+    try {
+        let imgElements = $('.post-body__content').find('img');
+        let imgSrcs = [];
+        imgElements.each(function() {
+            let src = $(this).attr('src');
+            if (src) {
+                imgSrcs.push(src);
+            }
+        });
+        return imgSrcs;
+    } catch (e) {
+        console.log('error finding rumor image', e);
+        return '';
     }
-    return '';
+}
+
+function isRumorPage($) {
+    let title = $('head > title').text().toLowerCase();
+    return title.includes('rumour') || title.includes('daemon engine')
+}
+
+function urlMeetsCommunityPostPattern(url) {
+    //examples
+    //https://www.warhammer-community.com/2019/03/01/the-daemon-engine-1st-march-2019-2/
+    //https://www.warhammer-community.com/2019/04/23/the-rumour-engine-23rd-april-2019/
+    //https://www.warhammer-community.com/2016/11/16/the-rumour-engine/
+    let withoutDomain = url.replace('https://www.warhammer-community.com/', '');
+    let segments = withoutDomain.split('/');
+    if (segments.length < 4) {
+        return false;
+    }
+    let year = parseInt(segments[0]);
+    if (isNaN(year) || year < 2000 || year > 9999) {
+        return false
+    }
+    let month = parseInt(segments[1]);
+    if (isNaN(month) || month < 1 || month > 12) {
+        return false
+    }
+    let day = parseInt(segments[2]);
+    if (isNaN(day) || day < 1 || day > 31) {
+        return false
+    }
+
+    return true;
+
 }
 
 
@@ -107,23 +162,30 @@ function collectInternalLinks($) {
     // console.log(relativeLinks);
     relativeLinks.each(function() {
         let page = $(this).attr('href')
-        if (!(page in pagesVisited)) {
-            pagesToVisit.push(page);
+        if (page in pagesVisited) {
+            return;
         }
+        if (!urlMeetsCommunityPostPattern(page)) {
+            return;
+        }
+
+        pagesToVisit.push(page);
     });
 }
 
 async function saveResults() {
     return new Promise((resolve, reject) => {
         let timestamp = new Date().toISOString();
-        let filePath = `results/${timestamp}.txt`
+        let filePath = `results/${timestamp}.json`
         // create results string
-        let resultString = '';
-        rumourImages.forEach(function(imageUrl) {
-            resultString += imageUrl + '\n';
-        });
+        let resultString = {
+            timestamp,
+            numPagesVisited,
+            resultsFound: rumorPages.length,
+            pages: rumorPages
+        };
 
-        fs.appendFile(filePath, resultString, (err) => {
+        fs.appendFile(filePath, JSON.stringify(resultString, null, 2), (err) => {
             if(err) {
                 reject(err);
             }
